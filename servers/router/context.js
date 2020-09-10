04c914f7ee1ab117d5daf04f4ca6cd15f190f8ad
kimqudxkr <70612833+kimqudxkr@users.noreply.github.com>
2020-09-10 15:34:15 +0900

    2020-09-10 통계 수정 (#12)
    * BTBoard Add
    
    * modify&delete
    
    * complete modify and delete
    
    * get->post, checkPassword
    
    * 기능은 완성
    
    * 코드 최적화
    
    Co-authored-by: icoul <icoul35@gmail.com>

--- servers/router/context.js
+++ servers/router/context.js
@@ -4,22 +4,85 @@ const connection = require("../connection");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('content/context');
-});
-
-  
-router.get('/:no', function(req, res, next) {
-    var num = req.params.no;
+  if(req.query.no) {
+    var num = req.query.no;
     const query = 'SELECT * FROM btboard WHERE no='+num;
 
     connection.query(query, function(err, rows) {
-        if(!err) {
-            res.render('content/context',{result:rows});
-          } else {
-            console.log('query error : '+err);
-            res.send(err);
+      if(!err) {
+        console.log('파람받고 넘어옴')
+        res.render('content/context',{result:rows});
+      } else {
+        console.log('query error : '+err);
+        res.send(err);
           }
-        });
+      });
+  } else {
+    console.log('no param');
+    res.render('content/context');
+    }
+})
+
+router.get('/api/check', (req, res, next) => {
+  var password = req.query.password;
+  var no = req.query.no;
+  let para = 'y';
+
+  const query = `SELECT password FROM btboard WHERE no=${no}`;
+
+  connection.query(query, function(err, result) {
+    if(!err) {
+      if(password == result[0].password) {
+        console.log('query success');
+        res.send(para);
+        console.log('비밀번호 일치 : '+para);
+      } else  {
+        para='n';
+        res.send(para);
+        console.log('비밀번호 다름 : '+para);
+      }
+    } else {
+      console.log('query error : '+err);
+      res.send(err);
+    }
+  });
+})
+
+router.post('/api/modify', (req, res, next) => {
+  var gotT = req.body.title;
+  var gotC = req.body.context;
+  var gotD = req.body.date;
+  var num = req.body.no;
+  
+  let query = `UPDATE btboard SET
+               title='${gotT}', context='${gotC}', date='${gotD}'
+               WHERE no='${num}'`
+  connection.query(query , function(err, rows) {
+    if(!err) {
+      var log = 'sucess'
+      console.log('query success');
+      res.send(log);
+    } else {
+      console.log('query error : '+err);
+      res.send(err);
+    }
+  })
+})
+
+router.post('/api/delete', (req, res, next) => {
+  var num = req.body.no;
+  let query = `DELETE FROM btboard WHERE no=${num}`;
+
+  connection.query(query, function(err, rows) {
+    if(!err) {
+      var log = 'sucess'
+      console.log('query success');
+      res.send(log);
+    } else {
+      console.log('query error : '+err);
+      res.send(err);
+    }
+  });
 })
 
 module.exports = router;
